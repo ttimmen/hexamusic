cceadfbf08ee9a504226d079b12354a99c84b6b5
ttimmen <ttimmen@gmail.com>
2014-03-15 20:47:51 +0100

    Unity control cubes

--- projectie/js/controller.js
+++ projectie/js/controller.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
 
   var socket = io.connect('10.100.1.101:3000');
-  socket.on('ctrl', function (data) {
+  socket.on('projectie', function (data) {
+    //console.log(data['projectie'])
+    //console.log(data['projectie'][7]);
+    u.getUnity().SendMessage("MAINSCRIPT","SetScaleX",data['projectie'][0]/110);
+    u.getUnity().SendMessage("MAINSCRIPT","SetScaleY",data['projectie'][1]/130);
+    u.getUnity().SendMessage("MAINSCRIPT","SetScaleZ",data['projectie'][2]/120);
+    u.getUnity().SendMessage("MAINSCRIPT","SetSpacingX",data['projectie'][3]/50);
+    u.getUnity().SendMessage("MAINSCRIPT","SetSpacingY",data['projectie'][4]/66);
+    //u.getUnity().SendMessage("MAINSCRIPT","SetCubesX",data['projectie'][5]/1);
+    //u.getUnity().SendMessage("MAINSCRIPT","SetCubesY",data['projectie'][6]/1);
+    u.getUnity().SendMessage("MAINSCRIPT","SetColor",data['projectie'][7]);
     u.getUnity().SendMessage("MAINSCRIPT", "Beat", "");
   });
 
