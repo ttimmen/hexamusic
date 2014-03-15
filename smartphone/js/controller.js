f1e00d898f5c54122e7c3f1be5ba4dbd549ddd08
Wim Notredame <Wim.notredame@vmma.be>
2014-03-15 18:35:26 +0100

    first changes with sockets
    Signed-off-by: Wim Notredame <Wim.notredame@vmma.be>

--- smartphone/js/controller.js
+++ smartphone/js/controller.js
@@ -11,20 +11,45 @@ $(document).ready(function() {
         $(this).html('');
 
     };
+    var updateBackground = function(bgcolor){
+        $('#color').css({
+            'background-color': bgcolor,
+        });
+
+    }
+
+
+
+    console.log($('#color').width());
     // Initialise the color picker
     $('#color').chromoselector({
         target: '#picker',
         autoshow: true,
-        width: 200,
+        width: $('#color').width(),
         preview: false,
         create: updatePreview,
         update: updatePreview
     }).chromoselector('show', 1);
 
-  var socket = io.connect('http://node.thevoice.vtm.be/');
-  socket.on('news', function (data) {
+  var socket = io.connect('10.100.1.101:3000');
+  socket.on('ctrl', function (data) {
     console.log(data);
-    socket.emit('my other event', { my: 'data' });
+    console.log(data.bgcolor)
+    updateBackground(data.bgcolor);
+
+    //console.log(json.bgcolor);
+    //console.log(json);
+   socket.emit('my other event', { my: 'data' });
   });
+
+
+// var testObject = { 'one': 34, 'two': 23, 'three': 3 };
+// // Put the object into storage
+// localStorage.setItem('testObjects', JSON.stringify(testObject));
+// // Retrieve the object from storage
+// var retrievedObject = localStorage.getItem('testObjects');
+
+// console.log('retrievedObject: ', JSON.parse(retrievedObject));
+
 
 });
